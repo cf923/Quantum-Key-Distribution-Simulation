ac27c9737eeaffd4dcac8fdf9e16624cfc70a41f
cf923 <137310255+cf923@users.noreply.github.com>
2025-03-10 11:09:29 +0000

    Update Quantum_Key_Distribution_Simulation.c
    Bug fix (key not converted properly from sender_key if sender_key==receiver_key, same as the .py equivalent).
    Changed display to work with frontend_graphs_compiled.py
    Minor improvements:
    nbits, error_bound stored in data segment, reduces variables required to pass to function parameters. count could be added to data segment but global variables are bad ideas.
    Errors checked in main(). Reduces variables required to pass to function parameters and makes generate_key() cleaner.
    print_array function is cleaner than repeated for loops.

--- Quantum_Key_Distribution_Simulation.c
+++ Quantum_Key_Distribution_Simulation.c
@@ -3,14 +3,16 @@
 #include <stdint.h>
 #include <time.h>
 
-void sendsignal(uint8_t* sender_bases, uint8_t* sender_states, int nbits){
+static int nbits = 10;
+
+void sendsignal(uint8_t* sender_bases, uint8_t* sender_states){
 	for (int i=0; i<nbits; ++i){
 		sender_bases[i] = rand()&1;
 		sender_states[i] = rand()&1+2*sender_bases[i];
 	}
 }
 
-void eavesdrop(uint8_t eavesdropping, uint8_t* sender_bases, uint8_t* sender_states, int nbits, uint8_t* eavesdropper_bases, uint8_t* eavesdropper_states){
+void eavesdrop(uint8_t eavesdropping, uint8_t* sender_bases, uint8_t* sender_states, uint8_t* eavesdropper_bases, uint8_t* eavesdropper_states){
 	if (eavesdropping){
 		for (int i=0; i<nbits; ++i){
 		eavesdropper_bases[i] = rand()&1;
@@ -29,7 +31,7 @@ void eavesdrop(uint8_t eavesdropping, uint8_t* sender_bases, uint8_t* sender_sta
 	}
 }
 			
-void receive_signal(uint8_t* eavesdropper_bases, uint8_t* eavesdropper_states, int nbits, uint8_t* receiver_bases, uint8_t* receiver_states){
+void receive_signal(uint8_t* eavesdropper_bases, uint8_t* eavesdropper_states, uint8_t* receiver_bases, uint8_t* receiver_states){
 	for (int i=0; i<nbits; ++i){
 		receiver_bases[i] = rand()&1;
 		if (receiver_bases[i] == eavesdropper_bases[i]){
@@ -40,7 +42,7 @@ void receive_signal(uint8_t* eavesdropper_bases, uint8_t* eavesdropper_states, i
 	}
 }
 
-int recognize_error(uint8_t* sender_bases, uint8_t* sender_states, int nbits, uint8_t* receiver_bases, uint8_t* receiver_states){
+int recognize_error(uint8_t* sender_bases, uint8_t* sender_states, uint8_t* receiver_bases, uint8_t* receiver_states){
 	int errors = 0;
 	for (int i=0; i<nbits; ++i){
 		if ((sender_bases[i] == receiver_bases[i]) && (sender_states[i]!=receiver_states[i])){
@@ -50,25 +52,27 @@ int recognize_error(uint8_t* sender_bases, uint8_t* sender_states, int nbits, ui
 	return errors;
 }
 
-void generate_key(int test_errors, uint8_t* sender_states, uint8_t* receiver_states, int error_bound, uint8_t* sender_key, uint8_t* receiver_key, int count, int* equal_positions, uint8_t* key){
-	if (test_errors > error_bound){
-		return;
-	}
+void generate_key(uint8_t* sender_states, uint8_t* receiver_states, uint8_t* sender_key, uint8_t* receiver_key, int count, int* equal_positions, uint8_t* key){
 	for (int i=0; i<count; ++i){
 		sender_key[i] = sender_states[equal_positions[i]];
 		receiver_key[i] = receiver_states[equal_positions[i]];
 		if (sender_key[i] == receiver_key[i]){
-			key[i] = sender_key[i];
+			key[i] = sender_key[i]%2;
 		} else{
 			key[0] = 5;
 		}
 	}
 }
 
+void print_array(uint8_t* array, int size){
+	for (int i=0; i<size; ++i){
+		printf("%u ", array[i]);
+	}
+	printf("\n");
+}
 	
 
 int main() {
-	int nbits = 10;
 	uint8_t* sender_bases = (uint8_t*)malloc(nbits * sizeof(uint8_t)); //sizeof(uint8_t) is 1
 	uint8_t* sender_states = (uint8_t*)malloc(nbits * sizeof(uint8_t));
 	
@@ -88,13 +92,13 @@ int main() {
 	srand(time(NULL));
 
 
-	sendsignal(sender_bases, sender_states, nbits);
+	sendsignal(sender_bases, sender_states);
 
-	eavesdrop(eavesdropping, sender_bases, sender_states, nbits, eavesdropper_bases, eavesdropper_states);
+	eavesdrop(eavesdropping, sender_bases, sender_states, eavesdropper_bases, eavesdropper_states);
 
-	receive_signal(eavesdropper_bases, eavesdropper_states, nbits, receiver_bases, receiver_states);
+	receive_signal(eavesdropper_bases, eavesdropper_states, receiver_bases, receiver_states);
 
-	int test_errors = recognize_error(sender_bases, sender_states, nbits, receiver_bases, receiver_states);
+	int test_errors = recognize_error(sender_bases, sender_states, receiver_bases, receiver_states);
 
 	int* equal_positions = (int*)malloc(nbits*sizeof(int));
 	int count = 0;
@@ -104,57 +108,28 @@ int main() {
 			++count;
 		}
 	}
-
-	printf("sender bases: ");
-	for (int i=0; i<nbits; ++i){
-		printf("%u ", sender_bases[i]);
-	}
-	printf("\nsender states: ");
-	for (int i=0; i<nbits; ++i){
-		printf("%u ", sender_states[i]);
-	}
-	printf("\neavesdropper_bases: ");
-	for (int i=0; i<nbits; ++i){
-		printf("%u ", eavesdropper_bases[i]);
-	}
-	printf("\neavesdropper_states: ");
-	for (int i=0; i<nbits; ++i){
-		printf("%u ", eavesdropper_states[i]);
-	}
-	printf("\nreceiver bases: ");
-	for (int i=0; i<nbits; ++i){
-		printf("%u ", receiver_bases[i]);
-	}
-	printf("\nreceiver states: ");
-	for (int i=0; i<nbits; ++i){
-		printf("%u ", receiver_states[i]);
-	}
-	printf("\ndetected %d errors\n", test_errors);
-	if (count == 0){
-		printf("No key established - no matching bases or error threshold too high");
-	} else{
-		int error_bound = 0;
+	print_array(sender_bases, nbits);
+	print_array(sender_states, nbits);
+	print_array(eavesdropper_bases, nbits);
+	print_array(eavesdropper_states, nbits);
+	print_array(receiver_bases, nbits);
+	print_array(receiver_states, nbits);
+	
+	static int error_bound = 0;
+	if ((count != 0) && (test_errors<=error_bound)){
 		uint8_t* sender_key = (uint8_t*)malloc(count * sizeof(uint8_t));
                 uint8_t* receiver_key = (uint8_t*)malloc(count * sizeof(uint8_t));
                 uint8_t* key = (uint8_t*)malloc(count * sizeof(uint8_t));
-                generate_key(test_errors, sender_states, receiver_states, error_bound, sender_key, receiver_key, count, equal_positions, key);
-		printf("sender key: ");
-		for (int i=0; i<count; ++i){
-			printf("%u ", sender_key[i]);
-		}
-		printf("\nreceiver key: ");
-		for (int i=0; i<count; ++i){
-			printf("%u ", receiver_key[i]);
-		}
-		printf("\nkey: ");
-		for (int i=0; i<count; ++i){
-			printf("%u ", key[i]);
+                generate_key(sender_states, receiver_states, sender_key, receiver_key, count, equal_positions, key);
+		print_array(sender_key, count);
+		print_array(receiver_key, count);
+		if (key[0]!=5){
+			print_array(key, count);
 		}
 		free(sender_key);
                 free(receiver_key);
                 free(key);
 	}
-	printf("\n");
 	
 	free(sender_bases);
 	free(sender_states);
@@ -168,5 +143,3 @@ int main() {
 	free(equal_positions);
 	return 0;
 }
-
-
