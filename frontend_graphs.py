ad7d9426274ac129580bdc5b574ea8ed02637ad5
cf923 <137310255+cf923@users.noreply.github.com>
2025-03-14 16:03:12 +0000

    Update frontend_graphs.py
    Separated key from senderkey/receiverkey, added appropriate keys/legends for the graphs, x-axis is stated to be nbits (it's not drawn on as it really mucks with the formatting)

--- frontend_graphs.py
+++ frontend_graphs.py
@@ -6,6 +6,7 @@ import streamlit as st
 import Quantum_Key_Distribution_Simulation as qk
 
 cols = ["Red", "Blue", "Green", "Yellow"]
+states_bases_list = [["vertical", "horizontal", "backslash", "forwardslash"], ["Rectilinear", "Diagonal"]]
 colmap = ListedColormap(cols)
 
 def plot_arrays(arrays, titles, eavesdropactive, color_map, ncols=4):
@@ -20,9 +21,20 @@ def plot_arrays(arrays, titles, eavesdropactive, color_map, ncols=4):
         ax.set_xticks(arange(len(array)))
         ax.set_yticks([])
         ax.set_title(title)
-    plt.legend(handles=[Patch(color=cols[i], label=str(i)) for i in range(ncols)],
+    plt.legend(handles=[Patch(color=cols[i], label=states_bases_list[ncols==2][i]) for i in range(ncols)],
                bbox_to_anchor=(1, -0.5), borderaxespad=0, ncol=ncols)
     plt.tight_layout()
+    return fig
+
+def plotkey(key, color_map):
+    if len(key)==1:
+        key=[key]
+    fig, ax = plt.subplots(1, 1, figsize=(10, 0.6))
+    ax.imshow(key.reshape(1, -1), aspect='auto', cmap=color_map, vmin=0, vmax=3)
+    ax.set_xticks(arange(len(key)))
+    ax.set_yticks([])
+    ax.set_title("key generated")
+    plt.legend(handles=[Patch(color="Red",label=0), Patch(color="Blue",label=1)], bbox_to_anchor=(1, -0.6), borderaxespad=0, ncols=2)
     return fig
 
 def run(nbits, eavesdropactive, error_bound):
@@ -31,18 +43,17 @@ def run(nbits, eavesdropactive, error_bound):
     rec_bases, rec_states = qk.receive_signal(eavbases, eavstates, nbits)
     errors = qk.recognise_error(send_bases, send_states, rec_bases, rec_states)
     senderkey, receiverkey, key = qk.generate_key(errors, send_bases, send_states, rec_bases, rec_states, error_bound)
-
+    st.markdown("x-axes are bits")
     st.header("Bases:")
     st.pyplot(plot_arrays([send_bases, eavbases, rec_bases], ["Sender Bases", "Eavesdropper Bases", "Receiver Bases"], eavesdropactive, colmap, ncols=2))
     st.header("States:")
     st.pyplot(plot_arrays([send_states, eavstates, rec_states], ["Sender States", "Eavesdropper States", "Receiver States"], eavesdropactive, colmap))
     st.write(f"Detected  {errors} error"+"s"*(errors!=1))
+    if senderkey is not None:
+        st.markdown("#### Sender and Receiver keys generated:")
+        st.pyplot(plot_arrays([senderkey, receiverkey], ["Sender Key", "Receiver Key"], eavesdropactive, colmap))
     if key is not None:
-        st.header("Keys:")
-        st.pyplot(plot_arrays([senderkey, receiverkey, key], ["Sender Key", "Receiver Key", "Resulting Key"], eavesdropactive, colmap))
-    else:
+        st.pyplot(plotkey(key, colmap))
+    if key is None:
         st.markdown("### Keys not generated - error rate too high")
-        if senderkey is not None:
-            st.markdown("#### Sender and Receiver keys generated:")
-            st.pyplot(plot_arrays([senderkey, receiverkey], ["Sender Key", "Receiver Key"], eavesdropactive, colmap))
     return
