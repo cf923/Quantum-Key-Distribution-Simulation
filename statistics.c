fa9267de8887734ebcbd3eb17777b444cbcb0efe
cf923 <137310255+cf923@users.noreply.github.com>
2025-03-15 12:13:29 +0000

    Update statistics.c (weird error i do not understand)
    running this gives:
    
    for error_bound = 0:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.609200%
    percent mismatches between sender and receiver states: 53.071499%
    
    for error_bound = 100000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.609200%
    percent mismatches between sender and receiver states: 53.071499%
    
    for error_bound = 200000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.609200%
    percent mismatches between sender and receiver states: 53.071499%
    
    for error_bound = 300000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.609200%
    percent mismatches between sender and receiver states: 53.071499%
    
    for error_bound = 400000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.609200%
    percent mismatches between sender and receiver states: 53.071499%
    
    for error_bound = 500000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.609200%
    percent mismatches between sender and receiver states: 53.071499%
    
    for error_bound = 600000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.609200%
    percent mismatches between sender and receiver states: 53.071499%
    
    for error_bound = 700000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.633600%
    percent mismatches between sender and receiver states: 53.088600%
    
    for error_bound = 800000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.633600%
    percent mismatches between sender and receiver states: 53.088600%
    
    for error_bound = 900000:
    percent errors recorded (sender and receiver states differ but basis doesn't): 15.633600%
    percent mismatches between sender and receiver states: 53.088600%
    
    anyone know why? should it converge?

--- statistics.c
+++ statistics.c
@@ -76,49 +76,53 @@ int main() {
 	uint8_t* receiver_bases = (uint8_t*)malloc(nbits*sizeof(uint8_t));
 	uint8_t* receiver_states = (uint8_t*)malloc(nbits*sizeof(uint8_t));
 
-		
-	if (sender_bases == NULL || sender_states == NULL) {
-		fprintf(stderr, "malloc fail");
-		return 1;
-	}
-	
-	srand(time(NULL));
-
-
-	sendsignal(sender_bases, sender_states);
-
-	eavesdrop(eavesdropping, sender_bases, sender_states, eavesdropper_bases, eavesdropper_states);
-
-	receive_signal(eavesdropper_bases, eavesdropper_states, receiver_bases, receiver_states);
-
-	int test_errors = recognize_error(sender_bases, sender_states, receiver_bases, receiver_states);
+	uint8_t* sender_key = (uint8_t*)malloc(nbits * sizeof(uint8_t));
+	uint8_t* receiver_key = (uint8_t*)malloc(nbits * sizeof(uint8_t));
+	uint8_t* key = (uint8_t*)malloc(nbits * sizeof(uint8_t));
 
 	int* equal_positions = (int*)malloc(nbits*sizeof(int));
-	int count = 0;
-	int bad_send = 0;
-	for (int i=0; i<nbits; ++i){
-		if (sender_bases[i] == receiver_bases[i]){
-			equal_positions[count] = i;
-			++count;
+	int count;
+	int bad_send;
+	float bad_send_percent;
+	float test_error_percent;
+	int test_errors;
+	for  (int error_bound=0; error_bound<nbits; error_bound+=100000){	
+		if (sender_bases == NULL || sender_states == NULL) {
+			fprintf(stderr, "malloc fail");
+			return 1;
 		}
-		if (sender_states[i]!=receiver_states[i]){
-			++bad_send;
+		
+		srand(time(NULL));
+
+		sendsignal(sender_bases, sender_states);
+
+		eavesdrop(eavesdropping, sender_bases, sender_states, eavesdropper_bases, eavesdropper_states);
+
+		receive_signal(eavesdropper_bases, eavesdropper_states, receiver_bases, receiver_states);
+
+		test_errors = recognize_error(sender_bases, sender_states, receiver_bases, receiver_states);
+
+		count = 0;
+		bad_send = 0;
+		for (int i=0; i<nbits; ++i){
+			if (sender_bases[i] == receiver_bases[i]){
+				equal_positions[count] = i;
+				++count;
+			}
+			if (sender_states[i]!=receiver_states[i]){
+				++bad_send;
+			}
 		}
+		bad_send_percent = 100*(float)bad_send / nbits;
+		test_error_percent = 100*(float)test_errors / nbits;
+		//static int error_bound = 0;
+		if ((count != 0) && (test_errors<=error_bound)){
+			generate_key(sender_states, receiver_states, sender_key, receiver_key, count, equal_positions, key);
+		}
+		printf("for error_bound = %d:\n", error_bound);
+		printf("percent errors recorded (sender and receiver states differ but basis doesn't): %f%%\n", test_error_percent);
+		printf("percent mismatches between sender and receiver states: %f%%\n\n", bad_send_percent);
 	}
-	float bad_send_percent = 100*(float)bad_send / nbits;
-	float test_error_percent = 100*(float)test_errors / nbits;
-	static int error_bound = 0;
-	if ((count != 0) && (test_errors<=error_bound)){
-		uint8_t* sender_key = (uint8_t*)malloc(count * sizeof(uint8_t));
-		uint8_t* receiver_key = (uint8_t*)malloc(count * sizeof(uint8_t));
-		uint8_t* key = (uint8_t*)malloc(count * sizeof(uint8_t));
-		generate_key(sender_states, receiver_states, sender_key, receiver_key, count, equal_positions, key);
-		free(sender_key);
-		free(receiver_key);
-		free(key);
-	}
-	printf("percent errors recorded (sender and receiver states differ but basis doesn't): %f%%\n", test_error_percent);
-	printf("percent mismatches between sender and receiver states: %f%%\n", bad_send_percent);
 	free(sender_bases);
 	free(sender_states);
 
@@ -127,7 +131,12 @@ int main() {
 
 	free(receiver_bases);
 	free(receiver_states);
-	
+
 	free(equal_positions);
+
+	free(sender_key);
+	free(receiver_key);
+	free(key);
+
 	return 0;
 }
