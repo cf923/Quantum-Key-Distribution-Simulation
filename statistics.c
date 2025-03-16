25ea6d1c2086ad70eeeec442ce98eef97fac644b
cf923 <137310255+cf923@users.noreply.github.com>
2025-03-16 11:52:18 +0000

    Update statistics.c
    Add eavesdropping on/off, better interfacing.

--- statistics.c
+++ statistics.c
@@ -7,7 +7,6 @@ int main() {
 	int maxbits = 100;
 	uint8_t* sender_bases = (uint8_t*)malloc(maxbits * sizeof(uint8_t)); //sizeof(uint8_t) is 1
 	uint8_t* sender_states = (uint8_t*)malloc(maxbits * sizeof(uint8_t));
-	int eavesdropping = 1;
 	uint8_t* eavesdropper_bases = (uint8_t*)malloc(maxbits * sizeof(uint8_t));
 	uint8_t* eavesdropper_states = (uint8_t*)malloc(maxbits * sizeof(uint8_t));
 	uint8_t* receiver_bases = (uint8_t*)malloc(maxbits*sizeof(uint8_t));
@@ -33,69 +32,72 @@ int main() {
 	int keygen_count;
 	srand(time(NULL));
 	int repeats = 10000;
-	for (int nbits=3; nbits<=maxbits; ++nbits){
-		keygen_count = 0;
-		bad_send = 0;
-		errors = 0;
-		for (int reps=0; reps<repeats; ++reps){
-			for (int i=0; i<nbits; ++i){
-				sender_bases[i] = rand()&1;
-				sender_states[i] = rand()&1+2*sender_bases[i];
-			}
-			if (eavesdropping){
+	for (int eavesdropping=0; eavesdropping<2; ++eavesdropping){
+		for (int nbits=3; nbits<=maxbits; ++nbits){
+			keygen_count = 0;
+			bad_send = 0;
+			errors = 0;
+			for (int reps=0; reps<repeats; ++reps){
 				for (int i=0; i<nbits; ++i){
-				eavesdropper_bases[i] = rand()&1;
-					if (eavesdropper_bases[i] == sender_bases[i]){
+					sender_bases[i] = rand()&1;
+					sender_states[i] = rand()&1+2*sender_bases[i];
+				}
+				if (eavesdropping){
+					for (int i=0; i<nbits; ++i){
+					eavesdropper_bases[i] = rand()&1;
+						if (eavesdropper_bases[i] == sender_bases[i]){
+							eavesdropper_states[i] = sender_states[i];
+						} else{
+							eavesdropper_states[i] = rand()&1+2*eavesdropper_bases[i];
+						}
+					}
+				} else{
+					for (int i=0; i<nbits; ++i){
+						eavesdropper_bases[i] = sender_bases[i];
 						eavesdropper_states[i] = sender_states[i];
-					} else{
-						eavesdropper_states[i] = rand()&1+2*eavesdropper_bases[i];
 					}
 				}
-			} else{
+				count = 0;
 				for (int i=0; i<nbits; ++i){
-					eavesdropper_bases[i] = sender_bases[i];
-					eavesdropper_states[i] = sender_states[i];
-				}
-			}
-			count = 0;
-			for (int i=0; i<nbits; ++i){
-				receiver_bases[i] = rand()&1;
-				if (receiver_bases[i] == eavesdropper_bases[i]){
-					receiver_states[i] = eavesdropper_states[i];
-				} else {
-					receiver_states[i] = rand()&1+2*receiver_bases[i];
-				}
-				if ((sender_bases[i] == receiver_bases[i]) && (sender_states[i]!=receiver_states[i])){
-					++errors;
-					equal_positions[count] = i;
-					++count;
-				} else{
-					if (sender_bases[i] == receiver_bases[i]){
+					receiver_bases[i] = rand()&1;
+					if (receiver_bases[i] == eavesdropper_bases[i]){
+						receiver_states[i] = eavesdropper_states[i];
+					} else {
+						receiver_states[i] = rand()&1+2*receiver_bases[i];
+					}
+					if ((sender_bases[i] == receiver_bases[i]) && (sender_states[i]!=receiver_states[i])){
+						++errors;
 						equal_positions[count] = i;
 						++count;
-					}
-					if (sender_states[i]!=receiver_states[i]){
-						++bad_send;
-					}
-				}
-			}
-			if ((count != 0) && (test_errors<=error_bound)){
-				for (int i=0; i<count; ++i){
-					sender_key[i] = sender_states[equal_positions[i]];
-					receiver_key[i] = receiver_states[equal_positions[i]];
-					if (sender_key[i] == receiver_key[i]){
-						key[i] = sender_key[i]%2;
 					} else{
-						key[0] = 5;
+						if (sender_bases[i] == receiver_bases[i]){
+							equal_positions[count] = i;
+							++count;
+						}
+						if (sender_states[i]!=receiver_states[i]){
+							++bad_send;
+						}
 					}
 				}
-				if (key[0]!=5){
-					++keygen_count;
+				if ((count != 0) && (test_errors<=error_bound)){
+					for (int i=0; i<count; ++i){
+						sender_key[i] = sender_states[equal_positions[i]];
+						receiver_key[i] = receiver_states[equal_positions[i]];
+						if (sender_key[i] == receiver_key[i]){
+							key[i] = sender_key[i]%2;
+						} else{
+							key[0] = 5;
+						}
+					}
+					if (key[0]!=5){
+						++keygen_count;
+					}
 				}
 			}
+			keygen_count_percent = 100*(float)keygen_count/repeats;
+			printf("%f ", keygen_count_percent);
 		}
-		keygen_count_percent = 100*(float)keygen_count/repeats;
-		printf("%f ", keygen_count_percent);
+		printf("\n");
 	}
 	printf("\n");
 	free(sender_bases);
