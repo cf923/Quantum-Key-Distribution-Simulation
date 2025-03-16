b359b0b0665f90ca09bbcef89baadc3983accd3c
cf923 <137310255+cf923@users.noreply.github.com>
2025-03-16 11:50:41 +0000

    Update statistics_interface.py
    Added eavesdropper on/off, axes, key/legend, less bad and wrong.

--- statistics_interface.py
+++ statistics_interface.py
@@ -1,10 +1,15 @@
 import subprocess
 import matplotlib.pyplot as plt
-
-process = subprocess.run(['./statistics'], capture_output=True, text=True, check=True)
-output_lines = process.stdout.strip().split('\n')
-floats = []
-for line in output_lines:
-    if line.strip():
-        floats.extend(map(float, line.split()))
-plt.plot(range(len(floats)), floats)
+import streamlit as st
+def run():
+    process = subprocess.run(['./statistics'], capture_output=True, text=True, check=True)
+    output_lines = process.stdout.strip().split('\n')
+    no_eavesdropper_vals = [float(x) for x in output_lines[0].split()]
+    eavesdropper_vals = [float(x) for x in output_lines[1].split()]
+    fig = plt.figure()
+    plt.plot(range(len(no_eavesdropper_vals)), no_eavesdropper_vals, label="no eavesdropper")
+    plt.plot(range(len(eavesdropper_vals)), eavesdropper_vals, label="with eavesdropper")
+    plt.xlabel("number of bits")
+    plt.ylabel("percent of keys generated")
+    plt.legend(loc='best')
+    st.pyplot(fig)
